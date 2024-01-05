b0a653da1ce5e4582da5caacb9603f458dd9e647
davidwt5 <davidwtan75@gmail.com>
2024-01-05 18:13:06 +1100

    fixed time zone problem

--- src/cronJobs/CronJobs.js
+++ src/cronJobs/CronJobs.js
@@ -3,13 +3,14 @@ const { channelId, starOwner, starManager } = require('../../config.json');
 const { readFromDb, writeToDb } = require('../utils/DbUtils');
 
 function startCronJobs(client) {
-  cron.tz.setDefault('Australia/Melbourne');
+  console.log(new Date());
   startOfDayNotification(client);
   endOfDayNotification(client);
 }
 
+// The timezone that is being used is 9 hours behind Melbourne. Therefore we push it ahead 9 hours.
 function startOfDayNotification(client) {
-  const startOfDay = '0 9 * * *'
+  const startOfDay = '0 22 * * *' // 9AM AEDT - 11 hours time difference
   cron.schedule(startOfDay, () => {
     let channel = client.channels.cache.get(channelId);
     sendMessage(channel, `<@${starOwner}> Don't forget to set your task for the day :3`)
@@ -17,7 +18,7 @@ function startOfDayNotification(client) {
 }
 
 function endOfDayNotification(client) {
-  const endOfDay = '0 4 * * *'
+  const endOfDay = '0 17 * * *' // 4AM - 11 hours time difference
   cron.schedule(endOfDay, async () => {
     let channel = client.channels.cache.get(channelId);
 
@@ -25,7 +26,7 @@ function endOfDayNotification(client) {
     let task = await readFromDb("taskOfTheDay");
 
     // If task of day not empty then notifiy me and stephy saying task is not completed and close task
-    if(task.length !== 0) {
+    if (task.length !== 0) {
       sendMessage(channel, `<@${starOwner}> <@${starManager}> Seems like the task might have been lapsed D:`)
       await writeToDb("taskOfTheDay", "");
     }
